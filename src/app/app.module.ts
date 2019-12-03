8eac47598ff2718ca79c263ab8639b9dc78aa7b3
Jacobokia <cobs399@gmail.com>
2019-12-03 23:00:33 +0300

    add the account settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatLabel, MatRadioButton, MatFormFieldModule, MatTabsModule, MatFormFieldControl, MatInputModule } from '@angular/material'
+import {MatRadioModule} from '@angular/material/radio'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountSettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatTabsModule,
+    MatInputModule
+    
+
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
